eacbae92ca90dc4bcb01003b57b53350d3dcdc88
Korshukov Stepan <korshukovstepan@gmail.com>
2021-07-07 22:35:43 +0300

    add few blocks in mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,1 @@
-// HAMBURGER
-
-const hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger--active");
-});
-
-
+import "./modules/slider";
---
+++ src/js/modules/slider.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.promo-slider').slick({
+    arrows: false,
+});     
