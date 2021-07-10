cb1c7cc273bcc42cc0e4c9b95f89d4aa30e75d07
Korshukov Stepan <korshukovstepan@gmail.com>
2021-07-10 13:11:11 +0300

    add outgoing 320px menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 import "./modules/slider";
+
+
+// OUTGOING MENU
+
+const menu = document.querySelector(".outgoing"),
+      //menuItem = document.querySelectorAll(".header-menu__list-link"),
+      cross = document.querySelector(".outgoing-cross"),
+      hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("outgoing--active");
+});
+
+cross.addEventListener("click", () => {
+    menu.classList.remove("outgoing--active");
+});
+
+/* menuItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("header-menu__hamburger--active");
+    menu.classList.toggle("header-menu__nav--active");
+  });
+}); */
